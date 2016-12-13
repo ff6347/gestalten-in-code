9656fd1f0c9b434cb03c7e3542722a44f9cb2de3
Julia Hilt <julia.hilt@fh-potsdam.de>
2016-12-13 22:25:09 +0100

    Replaced magic numbers

--- grid/p5js/corridor/index.js
+++ grid/p5js/corridor/index.js
@@ -7,7 +7,7 @@ function setup() {
   stroke(15);
   line(width/2, 4*height/10, width/2, 6*height/10);
   
-  for (var x = 0; x <=500; x +=25){
+  for (var x = 0; x <=width; x +=width/20){
     line(x, 0, width/2, 4*height/10);
     line(x, height, width/2, 6*height/10);  
   }
