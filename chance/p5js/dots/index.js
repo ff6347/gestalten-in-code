3d9a7db6e0748f8dbc9e6e01c5fc4de90b5317e8
Julia Hilt <julia.hilt@fh-potsdam.de>
2016-12-17 15:52:09 +0100

    Renamed variable

--- chance/p5js/dots/index.js
+++ chance/p5js/dots/index.js
@@ -4,10 +4,10 @@ function setup() {
   background(98);
   noStroke(); 
   
-  for (var x1 = 0; x1 <=width; x1 +=random(0, width/10)){
+  for (var x = 0; x <=width; x +=random(0, width/10)){
     fill(15);
-    ellipse(x1, random(height/4-20, height/4+20), width/20, height/20);
-    ellipse(x1, random(height/2-20, height/2+20), width/20, height/20);
-    ellipse(x1, random(3*height/4-20, 3*height/4+20), width/20, height/20);
+    ellipse(x, random(height/4-20, height/4+20), width/20, height/20);
+    ellipse(x, random(height/2-20, height/2+20), width/20, height/20);
+    ellipse(x, random(3*height/4-20, 3*height/4+20), width/20, height/20);
   }
 }
