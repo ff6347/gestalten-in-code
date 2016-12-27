7fa391845eadc1af12926caa93ad853318af0216
Daniele Maselli <danymase@gmail.com>
2016-12-27 13:21:55 +0100

    Color and similarity

---
+++ color/p5js/Law of simmilarity + color/index.js
@@ -0,0 +1,28 @@
+
+
+function setup() {
+  
+  createCanvas (800,800)
+  background (0);
+  
+
+}
+
+function draw() {
+  fill(65, 95, 98);
+  ellipse(200,200, 100, 100);
+  ellipse(600,600, 100, 100);
+  ellipse(200,600, 100, 100);
+  ellipse(600,200, 100, 100);
+  fill(354, 98, 95);
+  ellipse(400, 400, 100, 100);
+}
+
+/*
+[ ]
+{ 
+( )
+{ 
+( )
+
+*/
