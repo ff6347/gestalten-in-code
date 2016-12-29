5faca0f0263971d2352d6fe69193f1c4e94e9b94
Marius Claßen <marius.classen@gmail.com>
2016-12-29 21:36:25 +0100

    Create index.js

---
+++ motion/p5js/regularity/index.js
@@ -0,0 +1,20 @@
+    var angle = 0;
+    var offset = 250;
+    var scalar = 0;
+    var speed = 0.25;
+    
+    function setup() {
+      createCanvas(500, 500);
+      colorMode(HSB, 260, 100, 100, 100);
+      background (120, 25, 70, 100);
+      stroke(255);
+    }
+    function draw() {
+      var x = offset + cos(angle) * scalar;
+      var y = offset + sin(angle) * scalar;
+      var a = 90;
+      ellipse(x, y, 6, 6);
+      line(width/2, height/2, x, y);
+      angle += speed;
+      scalar += speed;
+    }
