cf881b9cfaa2c99758acc5302b87cf0f747abdd2
fabianmoronzirfas <icke@fabianmoronzirfas.me>
2017-01-02 15:47:23 +0100

    fix(loop): don't loop all the time and move some code to setup

--- form/3d/p5js/lines-for-space/index.js
+++ form/3d/p5js/lines-for-space/index.js
@@ -1,8 +1,11 @@
 function setup() {
-var canvas = createCanvas (500, 500);
-canvas.parent('sketch');
+  var canvas = createCanvas(500, 500);
+  canvas.parent('sketch');
 }
 
 function draw() {
-line(random(400), random(400), random(400), random(400));
+  line(random(400), random(400), random(400), random(400));
+  if(frameCount === 100) {
+    noLoop();
+  }
 }
