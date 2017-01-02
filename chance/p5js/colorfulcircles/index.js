174563cfd03f5cdfa537352334ba20dbf0d5b0e2
fabianmoronzirfas <icke@fabianmoronzirfas.me>
2017-01-02 20:31:45 +0100

    fix(encoding and line endings): fix windows encoding and line endings in index.md/js
    minor edits to get rid of to much random()

--- chance/p5js/colorfulcircles/index.js
+++ chance/p5js/colorfulcircles/index.js
@@ -1,44 +1,46 @@
-//size of full canvas
+// size of full canvas
 var canvas_size = 500;
 
-//starting size of circles
-//works best with being the same size as canvas_size
+// starting size of circles
+// works best with being the same size as canvas_size
 var circle_size = 500;
 
-//transparency range of all circles
+// transparency range of all circles
 var transparency_from = 50;
 var transparency_to = 100;
 
-//distance between all circles
+// distance between all circles
 var distance_from = 0;
 var distance_to = 3;
+var offset = 0;
 
-
-function setup() {   
+function setup() {
   var canvas = createCanvas(canvas_size, canvas_size);
   canvas.parent('sketch');
-  background(255);
+  colorMode(HSB, 360, 100, 100, 100);
+  background(100);
+  offset = random(360);
 }
 
 function draw() {
-    
-  var x_m = width/2;
-  var y_m = height/2;
-  
-  circle_size = circle_size - (random(distance_from, distance_to));
+
+  var x_m = width / 2;
+  var y_m = height / 2;
+
+  circle_size -= (random(distance_from, distance_to));
   var a = random(0, circle_size);
-  
-  if ( circle_size <= 0){
+
+  if (circle_size <= 0) {
     noLoop();
   }
-  
-  var cR = random(0, 255);
-  var cG = random(0, 255);
-  var cB = random(0, 255);
+
+  var cH = (random(120, 240) + offset) % 360;
+  var cS = random(50, 100) + circle_size;
+  var cB = random(50, 100);
   var cA = random(transparency_from, transparency_to);
-  fill(cR, cG, cB, cA);
+  fill(cH, cS, cB, cA);
   noStroke();
-  
+
   ellipse(x_m, y_m, a, a);
 }
 
--- chance/p5js/colorfulcircles/index.md
+++ chance/p5js/colorfulcircles/index.md
@@ -1,9 +1,10 @@
 ---  
 layout: p5js
-author: Malte V�lkner
+author: Malte Völkner
 author-url: https://github.com/MalteVoelkner
 title: Colorful circles
-tags: [P5.js, chance]
+tags: [P5.js, chance, repeat]
 permalink: /chance/colorfulcircles/
 summary: Creation of differently sized and colored circles that shrink with each loop
 ---
+Minor edits done by Fabian Morón Zirfas. Because to much random makes me go crazy!!!  
