fb649de734dca576d9f4646722d152927d4d5dd0
edgalindo <edmundo.mejia.galindo@fh-potsdam.de>
2017-01-09 22:02:03 +0100

    Adding a new Sample for Grid
    This Sample shows the Law of Contrast in a Grid

---
+++ grid/p5js/grid-contrast/sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+  createCanvas(500, 500);
+  background(220);
+  
+  var i = 0;
+  while (i < 500){
+    stroke(100);
+    line(i, i, width, i);
+    i = i + 2;
+  }
+  
+  noStroke();
+  fill(220);
+  rect(200, 200, 100, 100);
+  var y = 200;
+  while(y < 300){
+    stroke(100);
+    line(y, y, y, 300);
+    y += 2;
+  }
+}
+
+function draw() {
+  
+}
