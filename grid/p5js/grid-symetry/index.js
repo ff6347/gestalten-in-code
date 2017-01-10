c673b753824ba1a798e5decdb1e4516913ea4c22
edgalindo <edmundo.mejia.galindo@fh-potsdam.de>
2017-01-10 19:00:39 +0100

    Reviewing Code (;)
    Rewriting Code with (;)

--- grid/p5js/grid-symetry/index.js
+++ grid/p5js/grid-symetry/index.js
@@ -2,8 +2,7 @@ var a = 0;
 var b = 0;
 function setup() {
   createCanvas(500, 500);
-  background(255);
-  
+  background(255);  
 }
 
 function draw() {
@@ -13,7 +12,7 @@ function draw() {
   ellipseMode(RADIUS);
   noFill();
   ellipse(0, width/2, d, d);
-  ellipse(width, height/2, d, d)
+  ellipse(width, height/2, d, d);
     }
 }
 
