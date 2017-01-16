5cb40b53c244e31a0d51b76c5ea1d3e707c951eb
edgalindo <edmundo.mejia.galindo@fh-potsdam.de>
2017-01-16 17:43:01 +0100

    Files upload
    File has been uploaded, then the folder name was renamed and files were deleted.

---
+++ grid/p5js/fishing-net/index.js
@@ -0,0 +1,21 @@
+function setup() {
+   var canvas = createCanvas(500, 500);
+       canvas.parent('sketch');
+  background(255);
+  stroke(1);
+  
+  for(var i = 10; i < 500; i += 9){
+    stroke(1);
+    strokeWeight(i/50);
+    line(i, 0, i, 550);
+    line(500, i, 0, i);
+  }
+}
+
+function draw() {
+  
+}
+
+function mousePressed(){
+  save('out.png');
+}
