8f29f2849c6fb1f4e0ddf387d818a3f7f3115aeb
Daniele Maselli <danymase@gmail.com>
2017-02-13 17:14:58 +0100

    Create index.js

---
+++ color/p5js/colored-shapes/index.js
@@ -0,0 +1,51 @@
+var colors = [200, 100, 50, 20];
+
+function setup() {
+  var canvas = createCanvas(500, 500);  
+  canvas.parent('sketch');
+  background(255);
+}
+  
+
+function draw() {
+  var scaleFactor = 40; 
+  noStroke();
+ 
+  var x = 0;
+  var y = 0;
+
+  for (var i = 0, side = 30; side > 0; side--, i++) {
+    // fill with color
+    fill(colors[ i % colors.length ], 0, 0);
+    rect( x, y, scaleFactor * side, scaleFactor * side);
+  }
+
+  scaleFactor = 80; 
+  
+  var x = 800;
+	var y = 800; 
+ 	// loop for the radius of the circle
+  for (var i = 0, radius = 14; radius > 0; radius--, i++) {
+  	// fill with color
+ 		fill(0, 0, colors[i % colors.length]);
+    ellipse( x, y, scaleFactor * radius, scaleFactor * radius);
+  }
+ 
+
+  scaleFactor = 80;
+
+  var x = 0;
+  var y = 800;
+
+  for(var i = 0, radius = 14; radius > 0; radius--, i++) {
+    
+    fill(0,colors[i % colors.length], 0);
+    ellipse( x, y, scaleFactor * radius, scaleFactor * radius);
+
+  }
+
+ noLoop();
+
+
+}
+
