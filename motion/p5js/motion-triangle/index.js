bb4f29b939ca729de5dcb20cb50ddf0bbbf238aa
Daniele Maselli <danymase@gmail.com>
2017-02-13 21:09:32 +0100

    Update index.js

--- motion/p5js/motion-triangle/index.js
+++ motion/p5js/motion-triangle/index.js
@@ -1,56 +1,86 @@
+function cssColor(color){
+  return "rgb(" + color.r + "," + color.g + "," + color.b + ")";
+}
+colors = {
+  blue:   cssColor({ r: 0, g: 38, b: 255 }) ,
+  yellow: cssColor({ r: 240, g: 200, b: 0 }) ,
+  red :   cssColor({ r: 255, g: 0, b: 0 }),
+};
+
+
+var CANVAS_WIDTH  = 500;
+var CANVAS_HEIGHT = 500;
+
+
 // create new Vector object
-var movingVector = new p5.Vector(600, 300);
+var movingVector = new p5.Vector(100, 100);
 // create new Vector object
-var changeVector = new p5.Vector(5, 9);
+var changeVector = new p5.Vector(5, 10);
+
+scaleFactor = {
+  a : 1/2,
+  b : 15/16
+};
+
 
 function setup() {
-  var canvas = createCanvas(800, 800);
+  var canvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);     
   canvas.parent('sketch');
+  frameRate(120);
 }
 
 function draw() { 
   
   background(0); 
   noStroke();
-  frameRate(120);
 
   movingVector.add( changeVector );
 
-  if ( ( movingVector.x > width ) || ( movingVector.x < 0 ) ) {
+  if ( ( movingVector.x > CANVAS_WIDTH ) || ( movingVector.x < 0 ) ) {
     // change sign of v.x (change direction)
     changeVector.x = changeVector.x * -1;
   }
   
-  if ( ( movingVector.y > height ) || ( movingVector.y < 0 ) ) {
+  if ( ( movingVector.y > CANVAS_HEIGHT ) || ( movingVector.y < 0 ) ) {
     // change sign of v.y (change direction)
     changeVector.y = changeVector.y * -1;
   }
 
-  // draws the blue triangle when it is less than 400 on the right side. 
-  if( movingVector.x > 400 ){
-    fill(0, 38, 255); // blue
-    triangle(400, 0, movingVector.x, movingVector.y, 800, 750);
+
+  if( movingVector.x > (CANVAS_WIDTH * scaleFactor.a) ){
+    fill(colors.blue); // blue
+    triangle(CANVAS_WIDTH * scaleFactor.a, 0, movingVector.x, movingVector.y, CANVAS_WIDTH, CANVAS_HEIGHT * scaleFactor.b);
  
-    fill(255, 251, 0); // yellow
-    triangle(400, 0, movingVector.x, movingVector.y, 0, 750);
+    fill(colors.yellow); // yellow
+    triangle(CANVAS_WIDTH * scaleFactor.a, 0, movingVector.x, movingVector.y, 0, CANVAS_HEIGHT * scaleFactor.b);
 
     // ORDER: the blue triangle is under the yellow triangle
 
   } else {
     // draw first the yellow triangle
-    fill(255, 251, 0); // yellow
-    triangle(400, 0, movingVector.x, movingVector.y, 0, 750);
+    fill(colors.yellow); // yellow
+    triangle(CANVAS_WIDTH * scaleFactor.a, 0, movingVector.x, movingVector.y, 0, CANVAS_HEIGHT * scaleFactor.b);
     // then the blue triangle
-    fill(0, 38, 255); // blue
-    triangle(400, 0, movingVector.x, movingVector.y, 800, 750);
+    fill(colors.blue); // blue
+    triangle(CANVAS_WIDTH * scaleFactor.a, 0, movingVector.x, movingVector.y, CANVAS_WIDTH, CANVAS_HEIGHT * scaleFactor.b);
 
     // ORDER: the yellow triangle is under the blue triangle
   }  
 
-  // if movingVector is in the upper boundry [0, 750) draw the red triangle
-  if ( movingVector.y < 750 ) {
-    fill(255, 0, 0);  // red    
-    triangle( 0, 750, movingVector.x, movingVector.y, 800, 750 );    
+  
+  if ( movingVector.y < (CANVAS_HEIGHT * scaleFactor.b) ) {
+    fill(colors.red);  // red    
+    triangle( 0, CANVAS_HEIGHT * scaleFactor.b, movingVector.x, movingVector.y, CANVAS_WIDTH, CANVAS_HEIGHT * scaleFactor.b );    
   } else { /* dont draw the red triangle */ } 
 
+
 }
+
+
+
+
+
+
+
+
+
