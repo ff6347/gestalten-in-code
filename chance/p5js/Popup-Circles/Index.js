2f81b7354ca0039711ada742e6a70ee0076ce56e
Daniele Maselli <danymase@gmail.com>
2017-02-17 11:22:39 +0100

    Create Index.js

---
+++ chance/p5js/Popup-Circles/Index.js
@@ -0,0 +1,34 @@
+var initialOffset    = 10;
+var offset           = 25;
+var globalCircleSize = 15;
+
+var col = {
+  r: 180,
+  g: 63,
+  b: 203
+}
+
+
+function setup() {
+   var canvas = createCanvas(500, 500); 
+   canvas.parent('sketch');
+   background(0);  
+}
+
+function draw() {
+        
+    circleSize = globalCircleSize;
+
+    strokeWeight(3);
+    
+    for(var y = initialOffset; y < height; y = y + offset ) {
+    for(var x = initialOffset; x < width; x = x + offset){
+
+        ellipse(x, y, circleSize, circleSize);
+        circleSize = Math.abs(randomGaussian(5, 10)) | 0;
+    }
+     
+    }
+}
+
+//play with strokeWeight :) 
