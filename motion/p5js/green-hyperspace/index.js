9bb5f046649cdf589f7846b48498debaaca22968
filipczekajlo <czekajlofilip@gmail.com>
2017-07-27 13:06:04 +0200

    Update index.js

--- motion/p5js/green-hyperspace/index.js
+++ motion/p5js/green-hyperspace/index.js
@@ -1,23 +1,22 @@
 var lines = [];
 
-
+function aline () {
+  this.x = random(0, width);
+  this.y = random(0, height);
+  this.display = function display () {
+    stroke(random(125), random(250), random(125), 255);
+    line(height/2, width/2, this.x, this.y);
+  };
+  this.move = function move() {
+    this.x = this.x + random(-3, 3);
+    this.y = this.y + random(-3, 3);
+  }
+}
 function setup() {
   createCanvas(500, 500);
   for (var l = 0; l < 650; l++) {
-    lines[l] = {
-      x: random(0, width),
-      y: random(0, height),
-      display: function() {
-        stroke(random(125), random(250), random(125), 255);
-        line(height/2, width/2, this.x, this.y);
-      },
-      move: function() {
-        this.x = this.x + random(-3, 3);
-        this.y = this.y + random(-3, 3);
-      }
-    }
+    lines[l] = new aline();
   }
-
 }
 
 function draw() {
