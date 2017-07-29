8596c9ecef4dcb25dab7564d8782d0b1ce1581d4
jakobvk <jakob.von.kietzell@fh-potsdam.de>
2017-07-29 04:16:41 +0200

    english / sketch id div / removed image saving part

--- motion/p5js/commonfateplanets/index.js
+++ motion/p5js/commonfateplanets/index.js
@@ -1,30 +1,31 @@
-// Kreis1
+// circle1
 var circleX;
 var circleY;
 var sizeX;
 var sizeY;
 
-// Kreis2
+// circle2
 var circleX2;
 var circleY2;
 var sizeX2;
 var sizeY2;
 
-// Kreis3
+// circle3
 var circleX3;
 var circleY3;
 
-//Kreis4
+//circle4
 var circleX4;
 var circleY4;
 
-//Kreis5
+//circle5
 var circleX5
 var circleY5
 
 
 function setup() {
-  createCanvas (500,500);
+  var canvas = createCanvas(500, 500); 
+  canvas.parent('sketch'); 
   colorMode (HSB, 360,100,100, 100);
   
   circleX = 0;
@@ -49,29 +50,29 @@ function draw() {
   background (255);
   noStroke ();
 
-//Kreis1
+//circle1
   circleX = circleX+1;
   circleY = circleY+1;
   fill (120, 30, 100,50);
   ellipse (circleX, circleY, sizeX,sizeY);
 
-//Kreis2  
+//circle2  
   circleX2 = circleX2+1;
   circleY2 = circleY2+1;
   fill (120, 30, 100,50);
   ellipse (circleX2, circleY2, sizeX2,sizeY2);
 
-//Kreis3
+//circle3
   circleY3 = circleY3+1.5;
   fill (20,40,100,100);
   ellipse (circleX3, circleY3, 20,20);
   
-//Kreis4
+//circle4
   circleX4 = circleX4-2;
   fill (50);
   ellipse (circleX4, circleY4, 10,10);
   
-//Kreis5
+//circle5
   circleX5 = circleX5+0.25;
   circleY5 = circleY5-0.25;
   fill (50,10);
@@ -80,7 +81,7 @@ function draw() {
   
   
    
-// Kreis1 
+// circle1 
 if (circleX <= 250) {
   sizeX = sizeX+0.25;
   sizeY = sizeY+0.25;
@@ -116,19 +117,19 @@ else if (circleX2 >300) {
   sizeY2 = sizeY2-0.25;
 }
 
-//Kreis3 
+//cirlce3 
 if (circleY3 >= 500) {
   circleX3 =250;
   circleY3 =0;
 }
 
-//Kreis4
+//circle4
 if (circleX4 <= 0) {
   circleX4 =500;
   circleY4 =250;
 }
 
-//Kreis5
+//circle5
 if (circleX5 >= width+60) {
   circleX5 =-60;
   circleY5 =560;
@@ -136,10 +137,6 @@ if (circleX5 >= width+60) {
   
 }
 
-function mousePressed () {
-  save ('image.png');
-  
-}
 
   
 
