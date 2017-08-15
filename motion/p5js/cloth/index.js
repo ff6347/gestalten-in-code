b19dbd16ceca2ee4f06bdbb7cfb54faec7e8a16d
Ludwig Frank <lufra2606@gmail.com>
2017-08-15 18:58:05 +0200

    add motion sketch

---
+++ motion/p5js/cloth/index.js
@@ -0,0 +1,74 @@
+/**
+* P5 js sketch based on De Hong Attractor by Paul Bourke
+* xn+1 = sin(a yn) - cos(b xn)
+* yn+1 = sin(c xn) - cos(d yn)
+*
+* Ludwig Frank
+* v2 - More organic
+**/
+
+
+let SCALING = 0.6
+let x2, y2
+
+let NOICE_A = 0.0
+    NOICE_B = 1.1
+    NOICE_C = -2.2
+    NOICE_D = 2.3
+    x = 0
+    y = 0
+    VALUE_A = 0.0
+    VALUE_B = 0.0
+    VALUE_C = 0.0
+    VALUE_D = 0.0
+    a = 1.4191403
+    b = -2.2841523
+    c = 2.4575403
+    d = -2.177196
+
+function setup () {
+    let canvas = createCanvas(windowWidth, windowHeight)
+    stroke(90)
+    init()
+}
+
+function generateValues () {
+    NOICE_A += .0008
+    NOICE_B += noise(VALUE_A) * 0.01
+
+    VALUE_A = noise(NOICE_A) * 2
+    VALUE_B = noise(NOICE_B) * 4
+
+    a = VALUE_A
+    b = VALUE_B
+    c = 2.4575403 
+    d = -2.177196
+}
+
+function init () {
+    background(240)
+}
+
+function draw () {
+    background(240)
+    generateValues()
+
+    for (var i = 0; i < 8000; i ++) {
+
+        x2 = sin(a * y) - cos(b * x)
+        y2 = sin(c * x) - cos(d * y)
+
+        x = x2
+        y = y2
+
+        point( 
+            map( x2, -SCALING, SCALING, height / 2, width / 2 ), 
+            map( y2 * noise(2, 4), -SCALING, SCALING, height / 2, width / 2 ) 
+        )
+    }
+}
+
+function windowResized() {
+    resizeCanvas( windowWidth, windowHeight )
+    init()
+}
