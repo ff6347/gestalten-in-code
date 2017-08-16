1c678b0190feb0d9387f5d734b3bfba3eeff5efd
fabianmoronzirfas <fabian.moron.zirfas@gmail.com>
2017-08-16 14:52:42 +0200

    lint it, add some ";" even though @LFra hates them To keep a consistant coding style throughout the page

--- chance/p5js/planets/index.js
+++ chance/p5js/planets/index.js
@@ -1,38 +1,37 @@
-var
-    PARTICLE_COUNT = 120
-    EDGES = 50
-    
-var f, x
+var PARTICLE_COUNT = 120;
+var EDGES = 50;
+
+var f, x;
 
 function setup() {
-    noStroke()
-    createCanvas(windowWidth, windowHeight)
+  noStroke();
+  var canvas = createCanvas(500, 500);
+  canvas.parent('sketch');
 }
 
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight)
-}
 
 function draw() {
-    fill(130)
-    randomSeed(40)
-    background(240)
-    f = 0
+  fill(130);
+  randomSeed(40);
+  background(240);
+  f = 0;
 
-    if (mouseIsPressed) restart()
+  if (mouseIsPressed) {
+    restart();
+  }
 
-    for (var i = 0; i <= PARTICLE_COUNT; i++) {
-        push()
+  for (var i = 0; i <= PARTICLE_COUNT; i++) {
+    push();
 
-        translate(width / 2, height / 2)
-        rotate(f)
+    translate(width / 2, height / 2);
+    rotate(f);
 
-        for (var j = -120; j < 80; j++) {
-            x = randomGaussian(15, j)
-            ellipse(x, randomGaussian(200, x / PARTICLE_COUNT), 1, 1)
-        }
-
-        f = TAU / EDGES * random(x)
-        pop()
+    for (var j = -120; j < 80; j++) {
+      x = randomGaussian(15, j);
+      ellipse(x, randomGaussian(200, x / PARTICLE_COUNT), 1, 1);
     }
+
+    f = TAU / EDGES * random(x);
+    pop();
+  }
 }
--- motion/p5js/cloth/index.js
+++ motion/p5js/cloth/index.js
@@ -8,67 +8,68 @@
 **/
 
 
-let SCALING = 0.6
-let x2, y2
+var SCALING = 0.6;
+var x2, y2;
 
-let NOICE_A = 0.0
-    NOICE_B = 1.1
-    NOICE_C = -2.2
-    NOICE_D = 2.3
-    x = 0
-    y = 0
-    VALUE_A = 0.0
-    VALUE_B = 0.0
-    VALUE_C = 0.0
-    VALUE_D = 0.0
-    a = 1.4191403
-    b = -2.2841523
-    c = 2.4575403
-    d = -2.177196
+var NOICE_A = 0.0;
+var NOICE_B = 1.1;
+var NOICE_C = -2.2;
+var NOICE_D = 2.3;
+var x = 0;
+var y = 0;
+var VALUE_A = 0.0;
+var VALUE_B = 0.0;
+var VALUE_C = 0.0;
+var VALUE_D = 0.0;
+var a = 1.4191403;
+var b = -2.2841523;
+var c = 2.4575403;
+var d = -2.177196;
 
-function setup () {
-    let canvas = createCanvas(windowWidth, windowHeight)
-    stroke(90)
-    init()
+function init () {
+  background(240);
 }
 
 function generateValues () {
-    NOICE_A += .0008
-    NOICE_B += noise(VALUE_A) * 0.01
+  NOICE_A += 0.0008;
+  NOICE_B += noise(VALUE_A) * 0.01;
 
-    VALUE_A = noise(NOICE_A) * 2
-    VALUE_B = noise(NOICE_B) * 4
+  VALUE_A = noise(NOICE_A) * 2;
+  VALUE_B = noise(NOICE_B) * 4;
 
-    a = VALUE_A
-    b = VALUE_B
-    c = 2.4575403 
-    d = -2.177196
+  a = VALUE_A;
+  b = VALUE_B;
+  c = 2.4575403;
+  d = -2.177196;
 }
 
-function init () {
-    background(240)
+function setup () {
+  var canvas = createCanvas(500, 500);
+  canvas.parent('sketch');
+  stroke(90);
+  init();
 }
 
 function draw () {
-    background(240)
-    generateValues()
+  background(240);
+  generateValues();
 
-    for (var i = 0; i < 8000; i ++) {
+  for (var i = 0; i < 8000; i++) {
 
-        x2 = sin(a * y) - cos(b * x)
-        y2 = sin(c * x) - cos(d * y)
+    x2 = sin(a * y) - cos(b * x);
+    y2 = sin(c * x) - cos(d * y);
 
-        x = x2
-        y = y2
+    x = x2;
+    y = y2;
 
-        point( 
-            map( x2, -SCALING, SCALING, height / 2, width / 2 ), 
-            map( y2 * noise(2, 4), -SCALING, SCALING, height / 2, width / 2 ) 
-        )
-    }
+    point(
+            map(x2, -SCALING, SCALING, height / 2, width / 2),
+            map(y2 * noise(2, 4), -SCALING, SCALING, height / 2, width / 2)
+        );
+  }
 }
 
 function windowResized() {
-    resizeCanvas( windowWidth, windowHeight )
-    init()
+  resizeCanvas(windowWidth, windowHeight);
+  init();
 }
