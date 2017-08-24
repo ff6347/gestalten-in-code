22461d5e58fcc2acaf824761e815e7d2f6da0e20
fabianmoronzirfas <fabian.moron.zirfas@gmail.com>
2017-08-24 09:57:12 +0200

    😡 so many missing thumbnails…

--- assets/js/project.js
+++ assets/js/project.js
@@ -2,16 +2,19 @@ $(document).ready(function() {
 
   // lets make use of the thumbs for mobile
   if ($(window).width() < 450) {
-  var images = $('img');
-  images.each(function(i, val){
-    var res = val.src.split('/');
-    if(res[res.length -1] !== 'splash.png'){
-    // old source
-    var oldsrc = val.src;
-    val.src = val.src.replace(/\.png/,'-thumb.png');
-    $(val).wrap('<a href="' + oldsrc +'"></a>')
-    }
+  // so many of the students didnt bother to create the thumbnails
+  // grrrrrrr
+  //
+  // var images = $('img');
+  // images.each(function(i, val){
+  //   var res = val.src.split('/');
+  //   if(res[res.length -1] !== 'splash.png'){
+  //   // old source
+  //   var oldsrc = val.src;
+  //   val.src = val.src.replace(/\.png/,'-thumb.png');
+  //   $(val).wrap('<a href="' + oldsrc +'"></a>')
+  //   }
 
-  });
-}
+  // });
+  }
 });
